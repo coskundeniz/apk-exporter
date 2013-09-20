bf1d7ce9eaea109a7df89d8e5ccd30603849518d
Coşkun Deniz <coskun.denize@gmail.com>
2013-09-20 09:56:48 +0300

    Added initial_ant_tasks script and updated README file

---
+++ initial_ant_tasks.py
@@ -0,0 +1,22 @@
+#! python
+# -*- coding: utf-8 -*-
+
+import os
+import xml.etree.ElementTree as ET
+
+def initial_ant_tasks():
+		
+		dirname = raw_input("Enter project directory: ")
+
+		os.chdir(dirname)
+		os.system("android update project -p .")
+		
+		build_xml_tree = ET.parse(os.path.join(dirname, 'build.xml'))
+		build_root = build_xml_tree.getroot()
+		
+		build_root.set('default', 'release')
+		build_xml_tree.write(os.path.join(dirname, 'build.xml'), 
+                             encoding="utf-8", xml_declaration=True)
+		
+if __name__ == '__main__':
+	initial_ant_tasks()
