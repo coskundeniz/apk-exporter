8359315f4d07bd317aa63412edd4efa6642e4096
Coşkun Deniz <coskun.denize@gmail.com>
2013-09-20 10:15:57 +0300

    fixed indentation

--- initial_ant_tasks.py
+++ initial_ant_tasks.py
@@ -6,17 +6,17 @@ import xml.etree.ElementTree as ET
 
 def initial_ant_tasks():
 		
-		dirname = raw_input("Enter project directory: ")
+    dirname = raw_input("Enter project directory: ")
 
-		os.chdir(dirname)
-		os.system("android update project -p .")
+    os.chdir(dirname)
+    os.system("android update project -p .")
 		
-		build_xml_tree = ET.parse(os.path.join(dirname, 'build.xml'))
-		build_root = build_xml_tree.getroot()
+    build_xml_tree = ET.parse(os.path.join(dirname, 'build.xml'))
+    build_root = build_xml_tree.getroot()
 		
-		build_root.set('default', 'release')
-		build_xml_tree.write(os.path.join(dirname, 'build.xml'), 
-                             encoding="utf-8", xml_declaration=True)
+    build_root.set('default', 'release')
+    build_xml_tree.write(os.path.join(dirname, 'build.xml'),
+                         encoding="utf-8", xml_declaration=True)
 		
 if __name__ == '__main__':
-	initial_ant_tasks()
+    initial_ant_tasks()
